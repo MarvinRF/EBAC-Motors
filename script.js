985de9ff1c42772ce96554ec2e89c9ed3f3936d6
MarvinRF <marvinvca@outlook.com>
2023-04-17 18:26:51 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+    $('#carrousel-imagens').slick({ autoplay: true });
+
+    $('.menu-hamburger').click(function () {
+        $('nav').slideToggle();                       //slide down e slideup no mesmo botão
+    })
+    $('#campo-telefone').mask('(00) 00000-0000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            veiculoDeInteresse: {
+                required: true
+            },
+            mensagem: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Campo obrigatorio',
+            telefone: 'campo obrigatorio'
+        },
+        submitHandler: function (form) {
+            console.log(form)
+        },
+        invalidHandler: function (evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+    $('.lista-veiculos button').click(function () {
+        const destino = $('#contato');
+        const nomeVeiculo = ($(this).parent().find('h3').text());
+
+        $('#veiculo-interesse').val(nomeVeiculo);
+
+
+        $('html').animate({
+            scrollTop: destino.offset().top
+        }, 1000)
+    })
+})
